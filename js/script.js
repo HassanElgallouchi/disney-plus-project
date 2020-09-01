2afa326fe8e45845bf0c437451da697ed82b5e7c
unknown <helgallouchi@gmail.com>
2020-09-01 09:33:16 +0200

    add comment and javascript file

--- js/script.js
+++ js/script.js
@@ -1,42 +1,30 @@
-// window.onscroll = function() {myFunction()};
-
-// function myFunction() {
-//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-//     document.getElementById("myP").className = "test";
-//   } else {
-//     document.getElementById("myP").className = "";
-//   }
-//}
-
-
+// declare variables
 let btnBottom = document.querySelector(".btn-bottom");
 let logo = document.querySelector(".logo");
 let menu = document.querySelector(".menu");
 let signup = document.querySelector(".signup");
 
+// function on scroll window
 window.onscroll = function () {
 
+    // condition if page height >= 700
     if (window.pageYOffset >= 700) {
-
         btnBottom.style.display = "block";
         btnBottom.style.position = "fixed";
         btnBottom.style.width = "100%";
-
     } else {
         btnBottom.style.display = "none";
-
     }
 
-
-
-        if ((window.pageYOffset) >= 400 && (window.innerWidth) >= 480) {
-            menu.style.background = "#090b13";
-            logo.style.display = "block";
-            signup.style.display = "block";
-        } else {
-            menu.style.background = "transparent";
-            logo.style.display = "none";
-            signup.style.display = "none";
-        }
+    // condition if page height >= 400 and page width >= 480
+    if ((window.pageYOffset) >= 400 && (window.innerWidth) >= 480) {
+        menu.style.background = "#090b13";
+        logo.style.display = "block";
+        signup.style.display = "block";
+    } else {
+        menu.style.background = "transparent";
+        logo.style.display = "none";
+        signup.style.display = "none";
+    }
 
 }
